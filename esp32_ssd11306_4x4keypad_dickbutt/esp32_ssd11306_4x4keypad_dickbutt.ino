42d66ff078d117675881344a5f9900ccac4e8e03
stoffeejr <chris@roadkill.org>
2017-02-18 22:46:34 -0800

    dickbutt checked in the wrong file the first time, fixing that

--- esp32_ssd11306_4x4keypad_dickbutt/esp32_ssd11306_4x4keypad_dickbutt.ino
+++ esp32_ssd11306_4x4keypad_dickbutt/esp32_ssd11306_4x4keypad_dickbutt.ino
@@ -1,121 +1,309 @@
-/*this keypad tutorial number one by omar tarek 29-06-2014 */
-byte h=0,v=0;    //variables used in for loops
-const unsigned long period=50;  //little period used to prevent error
-unsigned long kdelay=0;        // variable used in non-blocking delay 
-const byte rows=4;             //number of rows of keypad
-const byte columns=4;            //number of columnss of keypad
-const byte Output[rows]={15,16,18,19}; //array of pins used as output for rows of keypad
-const byte Input[columns]={2,0,4,5}; //array of pins used as input for columnss of keypad
-byte keypad() // function used to detect which button is used 
+/*  Keypadtest.pde
+ *
+ *  Demonstrate the simplest use of the  keypad library.
+ *
+ *  The first step is to connect your keypad to the
+ *  Arduino  using the pin numbers listed below in
+ *  rowPins[] and colPins[]. If you want to use different
+ *  pins then  you  can  change  the  numbers below to
+ *  match your setup.
+ *
+ */
+#include <Keypad.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+const byte ROWS = 4; // Four rows
+const byte COLS = 4; // Four columns
+// Define the Keymap
+char keys[ROWS][COLS] = {
+  {'0','1','2','3'},
+  {'4','5','6','7'},
+  {'8','9','.',','},
+  {'!','@','#','$'}
+};
+// Connect keypad ROW0, ROW1, ROW2 and ROW3 to these Arduino pins.
+byte rowPins[ROWS] = { 15, 16, 18, 19 };
+// Connect keypad COL0, COL1 and COL2 to these Arduino pins.
+byte colPins[COLS] = { 2, 0, 4, 5 }; 
+
+// Create the Keypad
+Keypad kpd = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
+//define dickbutt size and look
+#define DICKBUTT_HEIGHT 32 
+#define DICKBUTT_WIDTH  32 
+static const unsigned char PROGMEM dickbutt_bmp[] =
 {
- static bool no_press_flag=0;  //static flag used to ensure no button is pressed 
-  for(byte x=0;x<columns;x++)   // for loop used to read all inputs of keypad to ensure no button is pressed 
-  {
-     if (digitalRead(Input[x])==HIGH);   //read evry input if high continue else break;
-     else
-     break;
-     if(x==(columns-1))   //if no button is pressed 
-     {
-      no_press_flag=1;
-      h=0;
-      v=0;
-     }
-  }
-  if(no_press_flag==1) //if no button is pressed 
-  {
-    for(byte r=0;r<rows;r++) //for loop used to make all output as low
-    digitalWrite(Output[r],LOW);
-    for(h=0;h<columns;h++)  // for loop to check if one of inputs is low
-    {
-      if(digitalRead(Input[h])==HIGH) //if specific input is remain high (no press on it) continue
-      continue;
-      else    //if one of inputs is low
-      {
-          for (v=0;v<rows;v++)   //for loop used to specify the number of row
-          {
-          digitalWrite(Output[v],HIGH);   //make specified output as HIGH
-          if(digitalRead(Input[h])==HIGH)  //if the input that selected from first sor loop is change to high
-          {
-            no_press_flag=0;                //reset the no press flag;
-            for(byte w=0;w<rows;w++) // make all outputs as low
-            digitalWrite(Output[w],LOW);
-            return v*4+h;  //return number of button 
-          }
-          }
-      }
-    }
-  }
- return 50;
-}
-void setup() 
+  0x80, 0x00, 0x00, 0x01, // #                              #
+  0x00, 0x00, 0x00, 0x00, //                                 
+  0x00, 0x37, 0x00, 0x00, //           ## ###                
+  0x00, 0xC0, 0x80, 0x00, //         ##      #               
+  0x01, 0x00, 0x80, 0x00, //        #        #               
+  0x03, 0x00, 0x40, 0x00, //       ##         #              
+  0x07, 0x8F, 0x40, 0x00, //      ####   #### #              
+  0x06, 0xDD, 0x40, 0x00, //      ## ## ### # #              
+  0x07, 0x8B, 0x40, 0x00, //      ####   # ## #              
+  0x04, 0xE0, 0x40, 0x00, //      #  ###      #              
+  0x07, 0xFC, 0x40, 0x30, //      #########   #        ##    
+  0x04, 0x00, 0x40, 0x28, //      #           #        # #   
+  0x04, 0x00, 0x40, 0x40, //      #           #       #      
+  0x04, 0x00, 0x40, 0x48, //      #           #       #  #   
+  0x04, 0x03, 0x40, 0x88, //      #        ## #      #   #   
+  0x04, 0x01, 0x5F, 0x90, //      #         # # ######  #    
+  0x04, 0x01, 0x21, 0x1C, //      #         #  #    #   ###  
+  0x04, 0x01, 0x0F, 0xC6, //      #         #    ######   ## 
+  0x04, 0x01, 0x00, 0x2E, //      #         #          # ### 
+  0x02, 0x01, 0x00, 0x12, //       #        #           #  # 
+  0x02, 0x05, 0xC0, 0x1C, //       #      # ###         ###  
+  0x01, 0x07, 0x00, 0x10, //        #     ###           #    
+  0x00, 0x80, 0x00, 0x20, //         #                 #     
+  0x00, 0xC0, 0x00, 0x60, //         ##               ##     
+  0x00, 0x30, 0x00, 0xC0, //           ##            ##      
+  0x00, 0x1F, 0x6F, 0x00, //            ##### ## ####        
+  0x00, 0x14, 0x18, 0x00, //            # #     ##           
+  0x00, 0xD8, 0xE8, 0x00, //         ## ##   ### #           
+  0x00, 0xA8, 0x48, 0x00, //         # # #    #  #           
+  0x00, 0x40, 0x28, 0x00, //          #        # #           
+  0x00, 0x30, 0x18, 0x00, //           ##       ##           
+  0x80, 0x00, 0x00, 0x01, // #                              #
+};
+
+
+char inChar;
+String string;
+
+void setup()
 {
-  for(byte i=0;i<rows;i++)  //for loop used to make pin mode of outputs as output
-  {
-  pinMode(Output[i],OUTPUT);
-  }
-  for(byte s=0;s<columns;s++)  //for loop used to makk pin mode of inputs as inputpullup
-  {
-    pinMode(Input[s],INPUT_PULLUP);
-  }
-  Serial.begin(9600); //to use serial monitor we set the buad rate
+  Serial.begin(9600);
+    // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 128x32)
+  // init done
+  
+  // Show image buffer on the display hardware.
+  // Since the buffer is intialized with an Adafruit splashscreen
+  // internally, this will display the splashscreen.
+  // I kept this because it's got a cute flower
+  display.display();
+  delay(7000);
+  
+
 }
+
+  
 void loop()
 {
-  if(millis()-kdelay>period) //used to make non-blocking delay
+  char key = kpd.getKey();
+  if(key)  // Check for a valid key.
   {
-    kdelay=millis();  //capture time from millis function
-switch (keypad())  //switch used to specify which button
-   {
-            case 0:
-            Serial.println(1);
-       break;  
-            case 1:
-            Serial.println(2);
-       break;
-            case 2:
-            Serial.println(3);
-       break;
-            case 3:
-            Serial.println("F1");
-       break;
-            case 4:
-            Serial.println(4);
-       break;
-            case 5:
-            Serial.println(5);
-       break;
-            case 6:
-            Serial.println(6);
-       break;
-            case 7:
-            Serial.println("F2");
-       break;
-            case 8:
-            Serial.println(7);
-       break;
-            case 9:
-            Serial.println(8);
-       break;
-            case 10:
-            Serial.println(9);
-       break;
-            case 11:
-            Serial.println("F3");
-       break;
-            case 12:
-            Serial.println("Mode");
-       break;
-            case 13:
-            Serial.println(0);
-       break;
-            case 14:
-            Serial.println("Cancel");
-       break;
-            case 15:
-            Serial.println("Enter");
-       break;
-            default:
-            ;
+    switch (key)
+    {
+      case '0':
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0,10);
+  display.println("Fucking 0");
+  display.display();
+  display.startscrollright(0x00, 0x0F);
+  delay(2000);
+  display.stopscroll();
+  display.clearDisplay();
+// stupid "looks like walking" dickbutt
+  walkingdickbutt();
+//  delay(1000);
+  display.clearDisplay();
+        break;
+         case '1':
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0,10);
+  display.println("Fucking 1");
+  display.display();
+  display.startscrollright(0x00, 0x0F);
+  delay(2000);
+  display.stopscroll();
+        break;
+       case '2':
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0,10);
+  display.println("Fucking 2");
+  display.display();
+  display.startscrollright(0x00, 0x0F);
+  delay(2000);
+  display.stopscroll();
+        break;
+         case '3':
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0,10);
+  display.println("Fucking 3");
+  display.display();
+  display.startscrollright(0x00, 0x0F);
+  delay(2000);
+  display.stopscroll();
+        break;
+        case '4':
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0,10);
+  display.println("Fucking 4");
+  display.display();
+  display.startscrollright(0x00, 0x0F);
+  delay(2000);
+  display.stopscroll();;
+        break;
+      case '5':
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0,10);
+  display.println("Fucking 5");
+  display.display();
+  display.startscrollright(0x00, 0x0F);
+  delay(2000);
+  display.stopscroll();
+        break;
+         case '6':
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0,10);
+  display.println("Fucking 6");
+  display.display();
+  display.startscrollright(0x00, 0x0F);
+  delay(2000);
+  display.stopscroll();
+        break;
+      case '7':
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0,10);
+  display.println("Fucking 7");
+  display.display();
+  display.startscrollright(0x00, 0x0F);
+  delay(2000);
+  display.stopscroll();
+        break;
+        case '8':
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0,10);
+  display.println("Fucking 8");
+  display.display();
+  display.startscrollright(0x00, 0x0F);
+  delay(2000);
+  display.stopscroll();
+        break;
+        case '9':
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0,10);
+  display.println("Fucking 9");
+  display.display();
+  display.startscrollright(0x00, 0x0F);
+  delay(2000);
+  display.stopscroll();
+        break;
+        case '.':
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0,10);
+  display.println("Fucking .");
+  display.display();
+  display.startscrollright(0x00, 0x0F);
+  delay(2000);
+  display.stopscroll();
+        break;
+   case ',':
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0,10);
+  display.println("Fucking ,");
+  display.display();
+  display.startscrollright(0x00, 0x0F);
+  delay(2000);
+  display.stopscroll();
+        break;
+   case '!':
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0,10);
+  display.println("Fucking !");
+  display.display();
+  display.startscrollright(0x00, 0x0F);
+  delay(2000);
+  display.stopscroll();
+        break;
+   case '@':
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0,10);
+  display.println("Fucking @");
+  display.display();
+  display.startscrollright(0x00, 0x0F);
+  delay(2000);
+  display.stopscroll();
+        break;
+   case '#':
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0,10);
+  display.println("Fucking #");
+  display.display();
+  display.startscrollright(0x00, 0x0F);
+  delay(2000);
+  display.stopscroll();
+        break;
+   case '$':
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0,10);
+  display.println("Fucking $");
+  display.display();
+  display.startscrollright(0x00, 0x0F);
+  delay(2000);
+  display.stopscroll();
+        break;
+        default:
+        Serial.println(key);
+    }
+  }
+  {   
+      display.clearDisplay();
+      inChar = Serial.read();
+      string+=inChar;
+      display.setCursor(0,0);
+      display.print(string);
+      display.display();
+  }
+}
+
+  //make dickbutt walk
+//need to add more dickbutt poses
+void walkingdickbutt(void) {
+
+for (uint8_t i=0; i<4; i++){
+  display.clearDisplay();
+  display.drawBitmap(108, 0,  dickbutt_bmp, 32, 32, 1);
+  display.display();
+  delay(500);
+  display.clearDisplay();
+  display.drawBitmap(72, 0,  dickbutt_bmp, 32, 32, 1);
+  display.display();
+  delay(500);
+  display.clearDisplay();
+  display.drawBitmap(36, 0,  dickbutt_bmp, 32, 32, 1);
+  display.display();
+  delay(500);
+  display.clearDisplay();
+  display.drawBitmap(0, 0,  dickbutt_bmp, 32, 32, 1);
+  display.display();
+  delay(500);
+  display.clearDisplay();
 }
 }
-}
+
